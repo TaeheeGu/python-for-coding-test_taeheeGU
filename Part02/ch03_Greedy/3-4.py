1576798f5133f28eb184a303238f15f72ee6ba3f
TaeHee <71616468+TaeheeGu@users.noreply.github.com>
2022-01-25 12:25:20 +0000

    ch03_Greedy

--- Part02/ch03_Greedy/3-4.py
+++ Part02/ch03_Greedy/3-4.py
@@ -16,6 +16,28 @@ while True:
 print(count)
 
 '''
+n, k = map(int, input().split())
+
+count = 0
+
+while n >= k:
+  while n % k != 0: # N이 K로 나누어 떨어지지 않는다면
+    n -= 1
+    count += 1
+
+  n //= k
+  count += 1
+
+while n > 1: # n이 k보다 작은 경우 더 이상 나눌수 없어 1씩 뺀다
+  n -= 1
+  count += 1
+
+print(count)
+'''
+
+'''
+n, k = map(int, input().split())
+
 while True:
   target = (n//k)*k   # n보다 작은 k로 나누어 떨어지는 수
   count += (n-target) # target이 될 때까지 -1의 횟수
@@ -28,4 +50,6 @@ while True:
   n //= k
 
 count += (n-1) #마지막으로 남은 수에 대하여 1씩 빼기 
+
+print(count)
 '''
